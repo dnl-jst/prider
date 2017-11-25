b2e3c8bd83d49cef3c11a8256692e3584a6903f6
Daniel Jost <info@daniel-jost.de>
2017-11-25 06:28:04 +0100

    replace bower with yarn, change build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,37 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+var stylePaths = [
+  'node_modules/bootstrap/scss/bootstrap.scss',
+  'web/css/**/*.scss'
+];
+
+var jsPaths = [
+  'node_modules/jquery/dist/jquery.js',
+  'node_modules/popper.js/dist/umd/popper.js',
+  'node_modules/bootstrap/dist/js/bootstrap.js'
+];
 
 gulp.task('default', function() {
-    return gulp.start('styles');
+    return gulp.start('styles', 'scripts');
 });
 
 gulp.task('watch', function() {
-    gulp.watch('web/css/**/*.scss', ['styles']);
+    gulp.watch(stylePaths.concat(jsPaths), ['styles', 'scripts']);
 });
 
 gulp.task('styles', function() {
-  return gulp.src('web/css/**/*.scss')
+  return gulp.src(stylePaths)
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('web/css/'));
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('web/build/'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(jsPaths)
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('web/build/'));
 });
